39a2567805d51adce44b5a78fe88abe9175a17ee
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-27 12:25:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// Importing productNames from names.json file
+// Importing products from products.json file
 const productNames = JSON.parse(
     fs.readFileSync(`${__dirname}/data/names.json`)
 );
@@ -11,24 +11,25 @@ const productNames = JSON.parse(
 app.use(express.json())
 
 // GET endpoint for sending the products to client by id
-//Endpoint - /api/v1/names/:id
-app.get("/api/v1/names/:id",(req, res)=>{
-
-    const {id} = req.params;
-    const product = productNames.find((product)=>{
-        return product.id == id;
-    })
-    if(!product){
-        return res.status(404).json({"status": "failed", "message": "Not found!"})
+app.get('/api/v1/names/:id',(req,res)=>{
+    let {id} = req.params;
+    id*=1;
+    const productName = productNames.find(productName => productName.id===id);
+    if(!productName){
+        return res.status(404).send({
+            status: "failed",
+            message: "Not found!"
+        });
     }
-    res.status(200).json({
-        "status": "success",
-        "message": "Product name fetched successfully",
-        "data": {
-            "productName": product
+    else{
+    res.status(200).send({
+        status : 'success',
+        message : "Product name fetched successfully",
+        data: {
+            productName
         }
-    })
-})
-
+    });
+    }
+});
 
 module.exports = app;
