d4b2063743535c90980061ca0daaffc68e6631b0
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-27 12:27:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
-// Importing products from products.json file
+// Importing productNames from names.json file
 const productNames = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/names.json`)
+  fs.readFileSync(`${__dirname}/data/names.json`)
 );
 
 //Middlewares
-app.use(express.json())
+app.use(express.json());
 
 // GET endpoint for sending the products to client by id
-app.get('/api/v1/names/:id',(req,res)=>{
-    let {id} = req.params;
-    id*=1;
-    const productName = productNames.find(productName => productName.id===id);
-    if(!productName){
-        return res.status(404).send({
-            status: "failed",
-            message: "Not found!"
-        });
-    }
-    else{
+//Endpoint - /api/v1/names/:id
+app.get("/api/v1/names/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = productNames.filter(item => {
+    return item.id == Number(userId);
+});
+  
+  if (user.length !== 0) {
+    const resObj={productName:{...user[0]}};
     res.status(200).send({
-        status : 'success',
-        message : "Product name fetched successfully",
-        data: {
-            productName
-        }
+      status: "success",
+      message: "Product name fetched successfully",
+      data: resObj,
     });
-    }
+  } else {
+    res.status(404).send({ status: "failed", message: "Not found!" });
+  }
 });
 
 module.exports = app;
